73ed88e7795e611b663e4dd4a4769bc2269f9b7f
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-08-27 10:39:54 -0500

    Built out navbar and preliminary dashboard/home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
+import TripList from './components/Tours/TripList';
 
 import './App.css';
 import '../src/sass/index.scss';
@@ -12,11 +13,21 @@ function App() {
   const [isRegistered, setIsRegistered] = useState(false);
 
   return (
-    <div className="App">
-      {/* <Login /> */}
-      {/* <Register /> */}
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Login /> */}
+        {/* <Register /> */}
+        <Dashboard />
+      </div>
+
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/home" component={Dashboard} />
+        <Route path="/about" />
+        <Route path="/trips" component={TripList} />
+        <Route path="/contact" />
+      </Switch>
+    </Router>
   );
 }
 
