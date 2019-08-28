87a4f13cea9a27a4a90b0a8b91226d850b62ce41
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-08-28 15:32:44 -0500

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Trip from "./components/Tours/Trip";
-import ContactForm from './components/ContactForm';
-import Login from './components/LoginModal';
-import Dashboard from './components/Dashboard';
-import TripList from './components/Tours/TripList';
-​
-import './App.css';
-import '../src/sass/index.scss';
-​
+import Login from "./components/LoginModal";
+import Dashboard from "./components/Dashboard";
+import TripList from "./components/Tours/TripList";
+
+import "./App.css";
+import "../src/sass/index.scss";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Dashboard />
       </div>
-​
+
       <Switch>
-        <Route path="/login" component={Login}/>
+        <Route path="/login" component={Login} />
         <Route path="/home" component={Dashboard} />
         <Route path="/about" />
         <Route path="/trips" component={TripList} />
-        <Route path="/trip" component={Trip}/>
-        <Route path="/contact" component={ContactForm} />
+        <Route path="/trip" component={Trip} />
+        <Route path="/contact" />
       </Switch>
-​
     </Router>
   );
 }
-​
+
 export default App;
