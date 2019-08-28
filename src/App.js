d834ab25da6ba86077867d81312803e160a2f3d9
Bangstry <tylerwesleyquinn@gmail.com>
2019-08-28 00:40:39 -0400

    form component / do not merge needs review

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Trip from "./components/Tours/Trip";
 import Login from './components/Login';
 import Register from './components/Register';
+import ContactForm from './components/ContactForm';
 
 import './App.css';
 import '../src/sass/index.scss';
@@ -13,10 +14,12 @@ function App() {
   return (
     <Router>
       <Route path="/trip" component={Trip}/>
+      <Route path="/contact" component={ContactForm}/>
     <div className="App">
       <h1>Welcome to Wanderlust!</h1>
       <Login />
       <Register />
+      <ContactForm/>
     </div>
     </Router>
   );
---
+++ src/components/ContactForm.js
@@ -0,0 +1,103 @@
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+const ContactForm = props => {
+const [ formState, setFormState ] = useState({
+    firstName: " ", 
+    lastName: " ",
+    email: " ",
+    phone:" ",
+    help:" ",
+});
+const [ isUpdating,setIsUpdating ] = useState(false)
+useEffect(() => {
+setFormState(props.update)
+
+},[props.update])
+
+const changeHandler = e => {
+    setFormState({
+    ...formState, [e.target.name]: e.target.value
+    })
+}
+
+const submitHandler = e => {
+    e.preventDefault();
+    // axios
+    // // post.("")
+
+    // .then (res => {
+    //     console.log(res)
+    // })
+
+    // .catch(error => {
+    //     console.log("This is an error", error)
+    // })
+    // }
+
+    setFormState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        help: "",
+    })
+}
+    return(
+        <div>
+            <form onSubmit={submitHandler}>
+                <label>
+                    First Name
+                    <input type="text"
+                     name="name" 
+                     placeholder="First Name" 
+                     onChange={changeHandler} 
+                    //  value={formState.name}
+                     />
+                     </label>
+                <label>
+                    Last Name
+                    <input
+                     type="text"
+                      name="last" 
+                      placeholder="Last Name" 
+                      onChange={changeHandler} 
+                    //   value={formState.last}
+                      />
+                      </label>
+                <label>
+                    Email
+                    <input 
+                    type="email"
+                    name="email" 
+                    placeholder="Email" 
+                    onChange={changeHandler} 
+                    //  value={formState.email}
+                     />
+                     </label>
+                <label>
+                    Phone
+                    <input
+                     type="tel" 
+                     name="phone" 
+                     placeholder="Phone Number" 
+                     onChange={changeHandler} 
+                    //  value={formState.phone}
+                     />
+                     </label>
+                <label>
+                    Need help?
+                    <input
+                     type="text" 
+                     name="help" 
+                     placeholder="Please describe your issue." 
+                     onChange={changeHandler} 
+                    //  value={formState.help}
+                     />
+                     </label>
+                     <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default ContactForm;
