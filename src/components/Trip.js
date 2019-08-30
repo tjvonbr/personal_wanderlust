3c31e94ac2dec02aa28b6a65420ca1b2d963e3e3
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-08-30 15:43:20 -0500

    done

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,30 +1,19 @@
-<<<<<<< HEAD:src/components/Tours/Trip.js
-import React from 'react';
-import { Button, Card, Image } from 'semantic-ui-react';
-=======
 import React from "react";
-import styled from "styled-components";
-import Image from "./images/tripimg.jpg";
->>>>>>> 182346fd4705087b520cbdfdffe8f8d794ee2a17:src/components/Trip.js
+import { Button, Card, Image } from "semantic-ui-react";
 
-  const Trip = ({trip}) => {
-    return(
-    <Card 
-      className="trip-card"
-      style={cardStyle}
-      centered={true}>
+const Trip = ({ trip }) => {
+  return (
+    <Card className="trip-card" style={cardStyle} centered={true}>
       <Card.Content>
         <Card.Header>{trip.title}</Card.Header>
         <Card.Meta>Start Date: {trip.startDate}</Card.Meta>
         <Card.Meta>End Date: {trip.endDate}</Card.Meta>
         <Card.Meta>Cost: ${trip.cost}</Card.Meta>
-        <Card.Description>
-          {trip.description}
-        </Card.Description>
+        <Card.Description>{trip.description}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <div className='trip-btn-container'>
-          <Button 
+        <div className="trip-btn-container">
+          <Button
             floated="right"
             className="btn add-btn"
             icon="add"
@@ -32,21 +21,20 @@ import Image from "./images/tripimg.jpg";
             content="Add Trip"
             size="small"
             // onClick={() => props.fetchId(props.trip.id)}
-            >
-
-          </Button>
+          ></Button>
         </div>
       </Card.Content>
     </Card>
-    )}
+  );
+};
 
-    const cardStyle = {
-  width: '600px',
-  height: '300px',
-  margin: '20px auto',
-  border: '1px solid gray',
-  borderRadius: '3px',
-  boxShadow: '2px 2px lightgray'
-}
+const cardStyle = {
+  width: "600px",
+  height: "300px",
+  margin: "20px auto",
+  border: "1px solid gray",
+  borderRadius: "3px",
+  boxShadow: "2px 2px lightgray"
+};
 
 export default Trip;
